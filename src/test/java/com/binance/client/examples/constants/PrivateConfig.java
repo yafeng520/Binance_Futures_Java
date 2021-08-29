d527231ee4b3e59cabf39d1d88a7182febac97a4
yafeng520 <762895636@qq.com>
2021-08-29 19:48:09 +0800

    First commit

--- src/test/java/com/binance/client/examples/constants/PrivateConfig.java
+++ src/test/java/com/binance/client/examples/constants/PrivateConfig.java
@@ -2,7 +2,7 @@ package com.binance.client.examples.constants;
 
 public class PrivateConfig {
 
-    public static final String API_KEY = "XXXXXX";
+    public static final String API_KEY = "myapikey";
     public static final String SECRET_KEY = "XXXXXX";
 
 }
